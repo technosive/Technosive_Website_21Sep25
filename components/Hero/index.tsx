0c131179849348a1b5d0fa075b70b23726a3a008
Abdul <technosive@outlook.com>
2025-09-22 20:35:09 +0100

    adas

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -37,7 +37,7 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://outlook.office365.com/owa/calendar/AbdulSalamsCalendar@technosive.co.uk/bookings/"
+                    href="https://calendly.com/abdul-salamguk/30min"
                     target="_blank"
                     className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
